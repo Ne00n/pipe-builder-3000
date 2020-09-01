a7f928dc89fb87bd6b4b54ca4ae5ce85a8735b20
Neo <neo@tiefkuehler-madness.me>
2020-09-01 21:26:55 +0200

    renamed interfaces to pipe*

--- Class/pipe.py
+++ Class/pipe.py
@@ -1,4 +1,4 @@
-import subprocess, json, re
+import subprocess, time, json, re
 from Class.templator import Templator
 
 targets = []
@@ -25,13 +25,16 @@ class Pipe:
             #Remove old configs
             #self.cmd(data['IP'],'rm /etc/wireguard/*',False)
             #Parse configs
-            parsed = re.findall("^[A-Za-z0-9]+",configs, re.MULTILINE)
+            parsed = re.findall("^pipe[A-Za-z0-9]+",configs, re.MULTILINE)
             #Disable old configs
+            print("---",server,"---")
             for client in parsed:
                 #Stop Server
-                self.cmd(server,'systemctl stop wg-quick@'+client+' && systemctl disable wg-quick@'+client,False)
+                print("Stopping","pipe"+server,"on",client.replace("pipe",""))
+                self.cmd(server.replace("pipe",""),'systemctl stop wg-quick@'+client+' && systemctl disable wg-quick@'+client,False)
                 #Stop Client
-                self.cmd(client,'systemctl stop wg-quick@'+server+' && systemctl disable wg-quick@'+server,False)
+                print("Stopping",client,"on",server.replace("pipe",""))
+                self.cmd(client.replace("pipe","") ,'systemctl stop wg-quick@pipe'+server+' && systemctl disable wg-quick@pipe'+server,False)
 
     def run(self):
         global targets
@@ -39,6 +42,7 @@ class Pipe:
         self.prepare()
         T = Templator()
         print("Launching")
+        time.sleep(3)
         for server,data in targets.items():
             print('---',server,'---')
             #Generate Server private key
@@ -54,18 +58,18 @@ class Pipe:
                 serverConfig = T.genServer(subnet,start,port,privateServer.rstrip(),publicClient.rstrip())
                 #Put Server config
                 print('Creating',client,'on',server)
-                self.cmd(server,'echo "'+serverConfig+'" > /etc/wireguard/'+client+".conf",False)
+                self.cmd(server,'echo "'+serverConfig+'" > /etc/wireguard/pipe'+client+".conf",False)
                 #Resolve hostname
                 ip = subprocess.check_output(['resolveip','-s',server]).decode("utf-8")
                 #Generate Client config
                 clientConfig = T.genClient(ip.rstrip(),subnet,start,port,privateClient.rstrip(),publicServer.rstrip())
                 #Put Client config
                 print('Creating',server,'on',client)
-                self.cmd(client,'echo "'+clientConfig+'" > /etc/wireguard/'+server+".conf",False)
+                self.cmd(client,'echo "'+clientConfig+'" > /etc/wireguard/pipe'+server+".conf",False)
                 #Enable Server
-                self.cmd(server,'systemctl enable wg-quick@'+client+' && systemctl start wg-quick@'+client,False)
+                self.cmd(server,'systemctl enable wg-quick@pipe'+client+' && systemctl start wg-quick@pipe'+client,False)
                 #Enable Client
-                self.cmd(client,'systemctl enable wg-quick@'+server+' && systemctl start wg-quick@'+server,False)
+                self.cmd(client,'systemctl enable wg-quick@pipe'+server+' && systemctl start wg-quick@pipe'+server,False)
                 print('Done',client,'on',server)
                 start +=2
                 port +=1
